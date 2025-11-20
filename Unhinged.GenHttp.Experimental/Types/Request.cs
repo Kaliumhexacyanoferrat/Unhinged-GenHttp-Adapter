fc82be8304955f8c4bda0633bad3eb8c5bc7c181
Andreas Nägeli <andr.naegeli@gmail.com>
2025-11-20 23:06:05 +0100

    Modernize adapter

--- Unhinged.GenHttp.Experimental/Types/Request.cs
+++ Unhinged.GenHttp.Experimental/Types/Request.cs
@@ -2,42 +2,52 @@ using GenHTTP.Api.Infrastructure;
 using GenHTTP.Api.Protocol;
 using GenHTTP.Api.Routing;
 using GenHTTP.Engine.Shared.Types;
-using Unhinged;
+
+using Unhinged.GenHttp.Experimental.Server;
 
 namespace Unhinged.GenHttp.Experimental.Types;
 
 public sealed class Request : IRequest
 {
-    private RequestProperties? _Properties;
+    private readonly ResponseBuilder _responseBuilder;
 
-    private Query? _Query;
+    private bool _freshResponse = true;
 
-    private ICookieCollection? _Cookies;
+    private IServer? _Server;
+
+    private IClientConnection? _Client;
+    private IClientConnection? _LocalCLient;
+
+    private FlexibleRequestMethod? _Method;
+    private RoutingTarget? _Target;
+
+    private readonly RequestProperties _Properties = new();
+
+    private readonly Query _Query = new();
+
+    private readonly CookieCollection _Cookies = new();
 
     private readonly ForwardingCollection _Forwardings = new();
 
-    private Headers? _Headers;
+    private readonly Headers _Headers = new();
 
     #region Get-/Setters
 
-    public IRequestProperties Properties
-    {
-        get { return _Properties ??= new RequestProperties(); }
-    }
+    public IRequestProperties Properties => _Properties;
 
-    public IServer Server { get; }
+    public IServer Server => _Server ?? throw new InvalidOperationException("Request is not initialized yet");
 
     public IEndPoint EndPoint => throw new InvalidOperationException("EndPoint is not available as it is managed by Unhinged");
 
-    public IClientConnection Client { get; }
+    public IClientConnection Client => _Client ?? throw new InvalidOperationException("Request is not initialized yet");
 
-    public IClientConnection LocalClient { get; }
+    public IClientConnection LocalClient => _LocalCLient ?? throw new InvalidOperationException("Request is not initialized yet");
 
-    public HttpProtocol ProtocolType { get; }
+    public HttpProtocol ProtocolType { get; private set; }
 
-    public FlexibleRequestMethod Method { get; }
+    public FlexibleRequestMethod Method => _Method ?? throw new InvalidOperationException("Request is not initialized yet");
 
-    public RoutingTarget Target { get; }
+    public RoutingTarget Target => _Target?? throw new InvalidOperationException("Request is not initialized yet");
 
     public string? UserAgent => this["User-Agent"];
 
@@ -47,22 +57,13 @@ public sealed class Request : IRequest
 
     public string? this[string additionalHeader] => Headers.GetValueOrDefault(additionalHeader);
 
-    public IRequestQuery Query
-    {
-        get { return _Query ??= new Query(Connection); }
-    }
+    public IRequestQuery Query => _Query;
 
-    public ICookieCollection Cookies
-    {
-        get { return _Cookies ??= FetchCookies(Connection); }
-    }
+    public ICookieCollection Cookies => _Cookies;
 
     public IForwardingCollection Forwardings => _Forwardings;
 
-    public IHeaderCollection Headers
-    {
-        get { return _Headers ??= new Headers(Connection); }
-    }
+    public IHeaderCollection Headers => _Headers;
 
     // TODO: Wrap the request content received by the client
     // For now there is never content, Unhinged doesn't yet support requests with body
@@ -72,34 +73,62 @@ public sealed class Request : IRequest
     {
         get
         {
-            if (Connection.H1HeaderData.Headers.TryGetValue("Content-Type", out var contentType))
+            if (Headers.TryGetValue("Content-Type", out var contentType))
             {
                 return FlexibleContentType.Parse(contentType);
             }
-            
+
             // TODO: Unhinged does not support requests with body yet
             return null;
         }
     }
 
-    //private IExpressRequest InnerRequest { get; }
-    private Connection Connection { get; }
+    private Connection? Connection { get; set; }
 
     #endregion
 
     #region Initialization
 
-    public Request(IServer server, Connection connection)
+    public Request(ResponseBuilder responseBuilder)
     {
-        Server = server;
+        _responseBuilder = responseBuilder;
+    }
+
+    #endregion
+
+    #region Functionality
+
+    public IResponseBuilder Respond()
+    {
+        if (!_freshResponse)
+        {
+            _responseBuilder.Reset();
+        }
+        else
+        {
+            _freshResponse = false;
+        }
+
+        return _responseBuilder;
+    }
+
+    public UpgradeInfo Upgrade() => throw new NotSupportedException("Web sockets are not supported by Unhinged");
+
+    public void Configure(ImplicitServer server, Connection connection)
+    {
+        _Server = server;
+
         Connection = connection;
-        
+
         // todo: Unhinged only supports Http11
         ProtocolType = HttpProtocol.Http11;
-        
-        Method = FlexibleRequestMethod.Get(connection.H1HeaderData.HttpMethod);
-        Target = new RoutingTarget(WebPath.FromString(connection.H1HeaderData.Route));
-        
+
+        _Method = FlexibleRequestMethod.Get(connection.H1HeaderData.HttpMethod);
+        _Target = new RoutingTarget(WebPath.FromString(connection.H1HeaderData.Route));
+
+        _Headers.SetConnection(connection);
+        _Query.SetConnection(connection);
+
         if (connection.H1HeaderData.Headers.TryGetValue("forwarded", out var entry))
         {
             _Forwardings.Add(entry);
@@ -109,11 +138,11 @@ public sealed class Request : IRequest
             _Forwardings.TryAddLegacy(Headers);
         }
 
-        LocalClient = new ClientConnection(connection);
+        _LocalCLient = new ClientConnection(connection);
 
         // todo: potential client certificate is not exposed by unhinged
         // Unhinged does not support Tls
-        Client = _Forwardings.DetermineClient(null) ?? LocalClient;
+        _Client = _Forwardings.DetermineClient(null) ?? LocalClient;
     }
 
     private CookieCollection FetchCookies(Connection connection)
@@ -129,28 +158,24 @@ public sealed class Request : IRequest
         return cookies;
     }
 
-    #endregion
+    internal void Reset()
+    {
+        _Headers.SetConnection(null);
+        _Query.SetConnection(null);
 
-    #region Functionality
-
-    public IResponseBuilder Respond() => new ResponseBuilder().Status(ResponseStatus.Ok);
-    
-    public UpgradeInfo Upgrade() => throw new NotSupportedException("Web sockets are not supported by Unhinged");
+        _Server = null;
+        _Client = null;
+        _LocalCLient = null;
+        _Method = null;
+    }
 
     #endregion
 
     #region Lifecycle
 
-    private bool _Disposed;
-
     public void Dispose()
     {
-        if (!_Disposed)
-        {
-            _Properties?.Dispose();
-
-            _Disposed = true;
-        }
+        // nop
     }
 
     #endregion
